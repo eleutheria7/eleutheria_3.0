847b82395cea1534519e46e82790b7173044d904
eleutheria7 <jovenseleutheria@gmail.com>
2025-04-14 23:05:19 -0300

    Update Contato.tsx

--- app/components/Contato.tsx
+++ app/components/Contato.tsx
@@ -10,8 +10,8 @@ export default function Contato() {
       <div>
         <h3 className="text-3xl font-bold text-center mb-12">Contato</h3>
         <Link href="https://wa.me/19981097482">
-          <h2 className="w-sm bg-green-600 text-white px-4 py-4 rounded-full hover:bg-green-700 transition-colors text-2xl font-semibold flex items-center justify-center">
-            Contatar o Organizador
+          <h2 className="250px bg-green-600 text-white px-4 py-4 rounded-full hover:bg-green-700 transition-colors text-2xl font-semibold flex items-center justify-center">
+            Falar com Organizador
           </h2>
         </Link>
       </div>
