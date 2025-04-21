024281da076071fcfdc94a99c1d25250f3c0309c
eleutheria7 <jovenseleutheria@gmail.com>
2025-04-21 09:32:26 -0300

    Update EleutheriaBanner.tsx

--- app/components/EleutheriaBanner.tsx
+++ app/components/EleutheriaBanner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function EleutheriaBanner() {
   return (
-    <section className="relative flex items-center justify-center text-white h-[40vh] min-h-[30vh] pt-20">
+    <section className="relative flex items-center justify-center text-white h-[40vh] min-h-[30vh] mt-20">
       <div className="flex items-center justify-center">
         <Image
           src="/assets/img/cabeçalho.jpg"
