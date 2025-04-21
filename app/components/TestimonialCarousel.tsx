bff8a16999329e53614ed94396d832147863813f
eleutheria7 <jovenseleutheria@gmail.com>
2025-04-21 09:45:11 -0300

    Update TestimonialCarousel.tsx

--- app/components/TestimonialCarousel.tsx
+++ app/components/TestimonialCarousel.tsx
@@ -11,12 +11,12 @@ import { text } from "stream/consumers";
 const testimonials = [
   {
     name: "Guilherme",
-    text: '"Me chamo Guilherme, tenho 23 anos, e havia me afastado de Deus por um longo tempo. Porém, o Eleutheria foi como uma guia para me reconectar e voltar para casa. No começo, senti um nervosismo de ir para um lugar onde não conhecia ninguém, mas, depois de fazer algumas novas amizades e viver tudo o que aconteceu lá, com tantas pessoas buscando reencontrar Deus e renovar sua fé, as pregações de pessoas incríveis me inspiraram muito. Hoje, não consigo mais imaginar minha vida fora da casa do Pai. Sem dúvida, participarei sempre que puder."',
+    text: '"Me chamo Guilherme, tenho 23 anos, e havia me afastado de Deus por um longo tempo. Porém, o Eleutheria foi como uma guia para me reconectar e voltar para casa. No começo, senti um nervosismo de ir para um lugar onde não conhecia ninguém, mas, depois de fazer algumas novas amizades e viver tudo o que aconteceu lá, com tantas pessoas buscando reencontrar Deus e renovar sua fé, as pregações de pessoas incríveis me inspiraram muito. Hoje, não consigo mais imaginar minha vida fora da casa do Pai. Sem dúvida, participarei sempre que puder."ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ',
     image: "/assets/img/Guilherme.jpg",
   },
   {
     name: "Maria Natalia",
-    text: '"Eu me chamo Maria Natália, tenho 25 anos e o Eleutheria foi o meu primeiro retiro. Não participei de outros retiros por medo e insegurança, mas estava afastada da fé e decidi que era hora de mudar. E essa foi a melhor escolha! Conheci pessoas novas, me senti muito acolhida e acima de tudo, me reencontrei com Deus! Foi uma experiência infinitamente maravilhosa! Sinto que minha vida mudou completamente, após o retiro! "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ',
+    text: '"Eu me chamo Maria Natália, tenho 25 anos e o Eleutheria foi o meu primeiro retiro. Não participei de outros retiros por medo e insegurança, mas estava afastada da fé e decidi que era hora de mudar. E essa foi a melhor escolha! Conheci pessoas novas, me senti muito acolhida e acima de tudo, me reencontrei com Deus! Foi uma experiência infinitamente maravilhosa! Sinto que minha vida mudou completamente, após o retiro! "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ',
     image: "/assets/img/Natalia.jpg",
   },
   {
@@ -26,17 +26,17 @@ const testimonials = [
   },
   {
     name: "Giovana Schicovski",
-    text: '"Meu nome é Giovana tenho 17 anos, participei do retiro Eleutheria ano passado, me reconectei profundamente com Jesus de uma forma que nunca imaginei. Foi ali que, com o coração aberto, me entreguei de uma vez por todas à Ele. Lembro com carinho de todos os momento sem que senti Sua presença de maneira tão forte, como tudo que estava ao meu redor sumisse e fosse apenas eu e ele. Eleutheria é muito mais que um retiro, é um tempo de cura e renovação. Cada oração, cada reflexão, cada momento de partilha, me aproximaram mais de Jesus e me ajudaram a entender o quanto Ele me ama."',
+    text: '"Meu nome é Giovana tenho 17 anos, participei do retiro Eleutheria ano passado, me reconectei profundamente com Jesus de uma forma que nunca imaginei. Foi ali que, com o coração aberto, me entreguei de uma vez por todas à Ele. Lembro com carinho de todos os momento sem que senti Sua presença de maneira tão forte, como tudo que estava ao meu redor sumisse e fosse apenas eu e ele. Eleutheria é muito mais que um retiro, é um tempo de cura e renovação. Cada oração, cada reflexão, cada momento de partilha, me aproximaram mais de Jesus e me ajudaram a entender o quanto Ele me ama."ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ',
     image: "/assets/img/Giovana Schicovski.jpg",
   },
   {
     name: "Bianca",
-    text: '"Me chamo Bianca, tenho 16 anos, e o Eleutheria foi um retiro que transformou minha vida. No começo, senti medo por não conhecer ninguém, mas decidi ir. E que escolha incrível! Lá, senti a presença de Deus de uma forma indescritível, vivi experiências que transformam qualquer pessoa e fiz amizades maravilhosas. O Eleutheria é muito mais que um retiro: é um lugar de renovação, fé e conversão. Sem dúvidas, voltarei muitas vezes!"',
+    text: '"Me chamo Bianca, tenho 16 anos, e o Eleutheria foi um retiro que transformou minha vida. No começo, senti medo por não conhecer ninguém, mas decidi ir. E que escolha incrível! Lá, senti a presença de Deus de uma forma indescritível, vivi experiências que transformam qualquer pessoa e fiz amizades maravilhosas. O Eleutheria é muito mais que um retiro: é um lugar de renovação, fé e conversão. Sem dúvidas, voltarei muitas vezes!"ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ',
     image: "/assets/img/Bianca.jpg",
   },
   {
     name: "Giovana Cruz",
-    text: '"Me chamo Giovana, tenho 18 anos e com certeza o Eleutheria foi a melhor experiência da minha vida. O retiro onde eu conseguia ver Jesus em todas as pessoas, tanto as que serviram e se doaram muito para estar ali, quanto nos retirantes também. Tive minha primeira experiência pessoal com Cristo no retiro e nunca esqueço de nenhum momento que vivi ali, das palestras, das orações, dos momentos de adoração, do sorriso de cada um, da comida muito boa nunca me senti tão bem, acolhida e tão feliz em toda minha vida, mesmo. Eleutheria é uma benção, e grande graça do Senhor foi participar."',
+    text: '"Me chamo Giovana, tenho 18 anos e com certeza o Eleutheria foi a melhor experiência da minha vida. O retiro onde eu conseguia ver Jesus em todas as pessoas, tanto as que serviram e se doaram muito para estar ali, quanto nos retirantes também. Tive minha primeira experiência pessoal com Cristo no retiro e nunca esqueço de nenhum momento que vivi ali, das palestras, das orações, dos momentos de adoração, do sorriso de cada um, da comida muito boa nunca me senti tão bem, acolhida e tão feliz em toda minha vida, mesmo. Eleutheria é uma benção, e grande graça do Senhor foi participar."ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ',
     image: "/assets/img/Giovana Cruz.jpg",
   },
   {
