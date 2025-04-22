15304bb367d0390b41176db2809456131ff9a31a
eleutheria7 <jovenseleutheria@gmail.com>
2025-04-21 23:17:47 -0300

    Update page.tsx

--- app/formulario/page.tsx
+++ app/formulario/page.tsx
@@ -512,7 +512,7 @@ export default function FormularioPage() {
             Enviar
           </button>
           {showPopup && (
-            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="fixed inset-0 flex items-center justify-center z-50">
               <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md text-center">
               <h2 className="text-xl font-bold text-green-600 mb-4">Formulário Enviado!</h2>
               <p className="text-gray-700">Seu cadastro foi recebido com sucesso. Nos vemos no Eleutheria 2025!</p>
